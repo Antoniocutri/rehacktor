ca5a8b7ee2076d022c6b519f350050de23dc7a38
Antonio <antoniocutri841@gmail.com>
2025-12-11 14:25:48 +0100

    create router, homepage and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider } from 'react-router'
 import './App.css'
+import router from './router/router'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
-      <button className="btn btn-warning">Warning</button>
+      <RouterProvider router={router}/>
     </>
   )
 }
---
+++ src/router/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import routes from "./routes"
+import Layout from "../components/layouts/Layout"
+import Homepage from "../views/Homepage"
+
+
+const router = createBrowserRouter([
+    {
+        path: routes.home,
+        Component: Layout,
+        children:[
+            {
+                path: routes.home,
+                Component: Homepage
+            }
+        ]
+    }
+])
+
+export default router;
