27fa00f1f26cf359a14596d694868aafc6d01559
Antonio <antoniocutri841@gmail.com>
2025-12-11 21:53:28 +0100

    create navbar and footer component and added in layout

---
+++ src/components/LayoutComponet/Footer.jsx
@@ -0,0 +1,11 @@
+import { FaBeer } from "react-icons/fa";
+
+export default function Footer() {
+    return(
+        <>
+            <div className="navbar bg-gray-400 shadow-sm mt-8">
+                <a className="btn btn-ghost text-xl"><FaBeer /></a>
+            </div>
+        </>
+    )
+}
--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "../LayoutComponet/Navbar";
+import Footer from "../LayoutComponet/Footer";
 
 export default function Layout() {
     return(
         <>
+            <Navbar/>
             <Outlet/>
+            <Footer/>
         </>
     )
 }
