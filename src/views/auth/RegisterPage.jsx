c5fd55a41b8e2c4b8e3263348ebd6ace67f57ba6
Antonio <antoniocutri841@gmail.com>
2025-12-12 16:20:30 +0100

    add onSubmit function to register user

--- src/views/auth/RegisterPage.jsx
+++ src/views/auth/RegisterPage.jsx
@@ -1,5 +1,7 @@
 import { useForm } from "react-hook-form";
 import { FaEnvelope, FaLock, FaUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import supabase from "../../database/supabase";
 
 export default function RegisterPage() {
 
@@ -9,7 +11,24 @@ export default function RegisterPage() {
         formState: { errors },
     } = useForm();
 
-    const onSubmit =(data)=>console.log(data);
+    const navigate = useNavigate()
+
+    const onSubmit = async (user_data)=>{
+        let {data, error} = await supabase.auth.signUp({
+            email: user_data.email,
+            password: user_data.password,
+            options: {
+                data: {
+                    first_name: user_data.first_name,
+                    last_name: user_data.last_name,
+                    username: user_data.username
+                }
+            }
+        })
+        console.log(user_data)
+        console.log(error)
+        navigate('/')
+    };
 
 
     return(
@@ -20,56 +39,87 @@ export default function RegisterPage() {
 
                     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                     
-                    <div>
-                        <label className="flex items-center gap-2 mb-1 font-medium"> <FaUser /> Nome </label>
-                        <input
-                            type="text"
-                            name="name"
-                            className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
-                            {...register("name", {
-                                required: "This film is required"
-                            })}
-                        />
-                        {errors.name && (
-                            <p role="alert" className="text-red-500 text-sm mt-1">{errors.name.message}</p>
-                        )}
-                    </div>
+                        <div>
+                            <label className="flex items-center gap-2 mb-1 font-medium"> <FaUser /> Nome </label>
+                            <input
+                                type="text"
+                                name="first_name"
+                                className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
+                                {...register("first_name", {
+                                    required: "This film is required"
+                                })}
+                            />
+                            {errors.first_name && (
+                                <p role="alert" className="text-red-500 text-sm mt-1">{errors.first_name.message}</p>
+                            )}
+                        </div>
 
-                    <div>
-                        <label className="flex items-center gap-2 mb-1 font-medium"> <FaEnvelope /> Email </label>
-                        <input
-                            type="email"
-                            name="email"
-                            className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
-                            {...register("email", {
-                                required: "This film is required"
-                            })}
-                        />
-                        {errors.email && (
-                            <p role="alert" className="text-red-500 text-sm mt-1">{errors.email.message}</p>
-                        )}
-                    </div>
+                        <div>
+                            <label className="flex items-center gap-2 mb-1 font-medium"> <FaUser /> Nome </label>
+                            <input
+                                type="text"
+                                name="last_name"
+                                className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
+                                {...register("last_name", {
+                                    required: "This film is required"
+                                })}
+                            />
+                            {errors.last_name && (
+                                <p role="alert" className="text-red-500 text-sm mt-1">{errors.last_name.message}</p>
+                            )}
+                        </div>
 
-                    <div>
-                        <label className="flex items-center gap-2 mb-1 font-medium"> <FaLock /> Password </label>
-                        <input
-                            type="password"
-                            name="password"
-                            className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
-                            {...register("password", {
-                                required: "This film is required"
-                            })}
-                        />
-                        {errors.password && (
-                            <p role="alert" className="text-red-500 text-sm mt-1">{errors.password.message}</p>
-                        )}
-                    </div>
+                        <div>
+                            <label className="flex items-center gap-2 mb-1 font-medium"> <FaUser /> Nome </label>
+                            <input
+                                type="text"
+                                name="username"
+                                className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
+                                {...register("username", {
+                                    required: "This film is required"
+                                })}
+                            />
+                            {errors.username && (
+                                <p role="alert" className="text-red-500 text-sm mt-1">{errors.username.message}</p>
+                            )}
+                        </div>
 
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-600 text-white py-3 rounded-xl shadow hover:bg-blue-700 transition">
-                        Registrati
-                    </button>
+                        <div>
+                            <label className="flex items-center gap-2 mb-1 font-medium"> <FaEnvelope /> Email </label>
+                            <input
+                                type="email"
+                                name="email"
+                                className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
+                                {...register("email", {
+                                    required: "This film is required"
+                                })}
+                            />
+                            {errors.email && (
+                                <p role="alert" className="text-red-500 text-sm mt-1">{errors.email.message}</p>
+                            )}
+                        </div>
+
+                        <div>
+                            <label className="flex items-center gap-2 mb-1 font-medium"> <FaLock /> Password </label>
+                            <input
+                                type="password"
+                                name="password"
+                                className="w-full border rounded-xl p-3 focus:ring focus:outline-none"
+                                {...register("password", {
+                                    required: "This film is required", 
+                                    minLength: 8,
+                                })}
+                            />
+                            {errors.password && (
+                                <p role="alert" className="text-red-500 text-sm mt-1">{errors.password.message}</p>
+                            )}
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-600 text-white py-3 rounded-xl shadow hover:bg-blue-700 transition">
+                            Registrati
+                        </button>
                     </form>
                 </section>
             </main>
